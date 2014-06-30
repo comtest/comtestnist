0816d3ca2d912f6db2c0b7c1c9305879074111d2
Wayman Tan <waymantan@gmail.com>
2014-06-30 11:31:08 -0400

    testing

--- citlab.importer.featureide/src/citlab/importer/featureide/MockClassificationTreeModel.java
+++ citlab.importer.featureide/src/citlab/importer/featureide/MockClassificationTreeModel.java
@@ -5,7 +5,7 @@ import java.util.Hashtable;
 import de.ovgu.featureide.fm.core.Feature;
 
 public class MockClassificationTreeModel {
-	private Feature root;
+	private Feature root;   
 	private Hashtable<String, Feature> nodeTable = new Hashtable<String, Feature>(); 
 	
 	public Hashtable<String, Feature> getNodeTable() {
