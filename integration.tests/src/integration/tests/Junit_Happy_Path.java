7cb50190a9f91e6ab3647141704f86431dd936d0
dkrew <dkrew0213@gmail.com>
2014-07-10 17:47:29 -0400

    Adding in the files for integration package

--- integration.tests/src/integration/tests/Junit_Happy_Path.java
+++ integration.tests/src/integration/tests/Junit_Happy_Path.java
@@ -5,12 +5,22 @@ package integration.tests;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
+import citlab.acts.IpoGGenerator;
+import citlab.acts.TSGeneratorByACTS;
+import citlab.core.ui.views.generator.TestSuiteView;
+import citlab.model.citL.CitLFactory;
+import citlab.model.citL.CitModel;
+import citlab.model.citL.Parameter;
+import citlab.model.test.ModelsFromFilesUtils;
+import citlab.testsuite.Assignment;
+import citlab.testsuite.TestSuite;
+import citlab.testsuite.TestsuiteFactory;
 import de.ovgu.featureide.fm.core.Feature;
 import de.ovgu.featureide.fm.core.FeatureModel;
 
@@ -35,7 +45,7 @@ public class Junit_Happy_Path {
 	}
 
 	@Test
-	public void test_Happy_Path() {
+	public void test_Happy_Path() throws IOException {
 		
 		System.out.println("Integration Test : test_Happy Path");
 		
@@ -52,9 +62,38 @@ public class Junit_Happy_Path {
 		
 		// Starting from the citlab
 		
+		TSGeneratorByACTS trans = new IpoGGenerator();
+		CitModel mobile_phone = ModelsFromFilesUtils.getBenchmark("mobile_phone.citl",true);
+		@SuppressWarnings("unused")
+		TestSuite ts = trans.generateTestsAndInfo(mobile_phone, true, true, true, 2);
+		
+		TestSuiteView view  = new TestSuiteView();
+		TestSuite after = view.add_in_expected_value_col(ts);
+		
+
+		for(int i = 0; i < after.getTests().get(0).getAssignments().size(); i++){
+			Assignment temp_assignemnt = after.getTests().get(0).getAssignments().get(i);
+			System.out.println(temp_assignemnt.getParameter().getName());
+		}
+		citlab.testsuite.Test temp_test = after.getTests().get(0);
+			
+			
+		assertEquals(2, temp_test.getAssignments().size());
+
+		
 		
 		
 
 	}
 
+	
+	@Test
+	public void test() throws IOException{
+		
+		TSGeneratorByACTS trans = new IpoGGenerator();
+		CitModel mobile_phone = ModelsFromFilesUtils.getBenchmark("mobile_phone.citl",true);
+		@SuppressWarnings("unused")
+		TestSuite ts = trans.generateTestsAndInfo(mobile_phone, true, true, true, 2);
+		
+	}
 }
