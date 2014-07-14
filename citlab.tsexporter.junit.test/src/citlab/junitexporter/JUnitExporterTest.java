af7eac5085f0b5ac687c0eb97934b52edf80fa09
Sudarshan Wadkar <wadkar@cmu.edu>
2014-07-14 14:11:21 -0400

    Organize imports

--- citlab.tsexporter.junit.test/src/citlab/junitexporter/JUnitExporterTest.java
+++ citlab.tsexporter.junit.test/src/citlab/junitexporter/JUnitExporterTest.java
@@ -3,11 +3,11 @@
  */
 package citlab.junitexporter;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.junit.After;
